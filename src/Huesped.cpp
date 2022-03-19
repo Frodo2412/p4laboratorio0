6578f09249f9d77c6258aa59978c0deb7ea24f35
Frodo2412 <bruno.lemus@bqn.com.uy>
2022-03-19 15:18:52 -0300

    Implementar Clase Huesped #1

---
+++ src/Huesped.cpp
@@ -0,0 +1,31 @@
+//
+// Created by brunolemus on 3/19/22.
+//
+
+#include "../include/Huesped.h"
+
+Huesped::Huesped(string nombre, string email, bool esFinger) {
+    this->nombre = nombre;
+    this->email = email;
+    this->esFinger = esFinger;
+}
+
+string Huesped::getNombre() {
+    return nombre;
+}
+
+string Huesped::getEmail() {
+    return email;
+}
+
+bool Huesped::isFinger() {
+    return esFinger;
+}
+
+void Huesped::setNombre(string nombre) {
+    this->nombre = nombre;
+}
+
+void Huesped::setFinger(bool esFinger) {
+    this->esFinger = esFinger;
+}

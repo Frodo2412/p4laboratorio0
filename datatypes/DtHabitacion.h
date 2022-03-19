072a83dc3b6780141a2bcf18b46b4addb3792563
Frodo2412 <bruno.lemus@bqn.com.uy>
2022-03-19 15:08:40 -0300

    Implementar DataType Habitacion #8

---
+++ datatypes/DtHabitacion.h
@@ -0,0 +1,22 @@
+//
+// Created by brunolemus on 3/19/22.
+//
+
+#ifndef P4LABORATORIO0_DTHABITACION_H
+#define P4LABORATORIO0_DTHABITACION_H
+
+
+class DtHabitacion {
+private:
+    int numero;
+    float precio;
+    int capacidad;
+public:
+    DtHabitacion(int, float, int);
+
+    int getNumero();
+    float getPrecio();
+    int getCapacidad();
+};
+
+#endif //P4LABORATORIO0_DTHABITACION_H

eed479db85aa84ecc3e8e92c6d9f1d9aa0fee99a
ElKufa <facundo.barboza@fing.edu.uy>
2022-03-19 20:37:22 -0300

    13 agregar huesped

--- src/Sistema.cpp
+++ src/Sistema.cpp
@@ -5,22 +5,38 @@ Sistema::Sistema() {
     int i = 0;
     while (i < MAX_HABITACIONES) {
         this->habitaciones[i] = nullptr;
+        this->huespedes[i] = nullptr;
         i++;
     }
-    tope = 0;
+    cantHabitaciones = 0;
+}
+
+void Sistema::agregarHuesped(string nombre, string email, bool esFinger) {
+    if (!isMailValido(email)) {
+        int i = 0;
+        bool salir = true;
+        while (salir) {
+            if (huespedes[i] == nullptr) {
+                salir = false;
+            } else
+                i++;
+        }
+        huespedes[i] = new Huesped(nombre, email, esFinger);
+    } else
+        throw std::invalid_argument("Email existente");
 }
 
 void Sistema::agregarHabitacion(int numero, float precio, int capacidad) {
     if (hayEspacioParaHabitacion() && numeroNoAsignadoAHabitacion(numero)) {
-        habitaciones[tope] = new Habitacion(numero, precio, capacidad);
-        tope++;
+        habitaciones[cantHabitaciones] = new Habitacion(numero, precio, capacidad);
+        cantHabitaciones++;
     }
 }
 
 bool Sistema::numeroNoAsignadoAHabitacion(int numero) {
     int i = 0;
     bool res = true;
-    while (i < tope && res) {
+    while (i < cantHabitaciones && res) {
         if (habitaciones[i]->getNumero() == numero)
             res = false;
         i++;
@@ -31,17 +47,28 @@ bool Sistema::numeroNoAsignadoAHabitacion(int numero) {
 }
 
 bool Sistema::hayEspacioParaHabitacion() {
-    return tope < MAX_HABITACIONES;
+    return cantHabitaciones < MAX_HABITACIONES;
 }
 
 DtHabitacion **Sistema::obtenerHabitaciones(int &cantHabitaciones) {
-    DtHabitacion **habs = new DtHabitacion *[tope];
+    DtHabitacion **habs = new DtHabitacion *[cantHabitaciones];
     int i = 0;
-    while (i < tope) {
+    while (i < cantHabitaciones) {
         habs[i] = new DtHabitacion(this->habitaciones[i]);
         i++;
     }
-    cantHabitaciones = tope;
+    cantHabitaciones = this->cantHabitaciones;
     return habs;
 }
 
+bool Sistema::isMailValido(string email) {
+    int i = 0;
+    bool aparece = false;
+    while (huespedes[i] != nullptr && !aparece) {
+        if (huespedes[i]->getEmail() == email) {
+            aparece = true;
+        }
+        i++;
+    }
+    return aparece;
+}

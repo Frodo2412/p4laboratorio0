89835809a51bce1f8a5e4f4ec9ce03d5e6470534
Frodo2412 <bruno.lemus@bqn.com.uy>
2022-03-19 14:17:36 -0300

    Implementar DataType DtFecha #9

---
+++ datatypes/DtFecha.cpp
@@ -0,0 +1,17 @@
+//
+// Created by brunolemus on 3/19/22.
+//
+
+#include "DtFecha.h"
+
+int DtFecha::getDia() {
+    return dia;
+}
+
+int DtFecha::getMes() {
+    return mes;
+}
+
+int DtFecha::getAnio() {
+    return anio;
+}

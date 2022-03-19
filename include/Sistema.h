a1475a03506be1f3894e321843eb979973f4ab7d
Frodo2412 <bruno.lemus@bqn.com.uy>
2022-03-19 14:00:38 -0300

    Initial Commit

---
+++ include/Sistema.h
@@ -0,0 +1,14 @@
+//
+// Created by brunolemus on 3/19/22.
+//
+
+#ifndef P4LABORATORIO0_SISTEMA_H
+#define P4LABORATORIO0_SISTEMA_H
+
+
+class Sistema {
+
+};
+
+
+#endif //P4LABORATORIO0_SISTEMA_H
---
+++ src/Sistema.cpp
@@ -0,0 +1,5 @@
+//
+// Created by brunolemus on 3/19/22.
+//
+
+#include "../include/Sistema.h"
